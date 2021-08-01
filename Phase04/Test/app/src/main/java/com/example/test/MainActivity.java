7c435d3920d110f91da794b8da3d8097fc9a1074
tahamousavi <tahamousavi.sbu@gmail.com>
2021-08-02 00:25:01 +0430

    Phase 4 started

---
+++ Phase04/Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Talkademy", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
